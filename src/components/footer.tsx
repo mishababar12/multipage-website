925f769921c92c4492e03234a9c7578d09ad9aeb
mishababar12 <160350080+mishababar12@users.noreply.github.com>
2025-02-03 04:57:40 -0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
   return (
-    <footer className=" bg-gray-800 text-white text-center p-4 ">
+    <div className=" bg-gray-800 text-white text-center p-4 ">
       <p>&copy; 2025 Crockery Shop. All rights reserved.</p>
-    </footer>
+    </div>
   );
 };
 
